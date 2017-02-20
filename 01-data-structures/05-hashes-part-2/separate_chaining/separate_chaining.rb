cbbb015768d63100afda4f82cf078424a25fac5f
Forrest Stone <iloveburningman@gmail.com>
2017-02-19 23:24:30 -0800

    not much

--- 01-data-structures/05-hashes-part-2/separate_chaining/separate_chaining.rb
+++ 01-data-structures/05-hashes-part-2/separate_chaining/separate_chaining.rb
@@ -2,15 +2,21 @@ require_relative 'linked_list'
 
 class SeparateChaining
   attr_reader :max_load_factor
-
+  attr_reader :ray_o_lists
 
 
 
   def initialize(size)
     @max_load_factor = 0.7
+    @inserts = 0
+    @ray_o_lists =[]
   end
 
   def []=(key, value)
+
+
+
+    @inserts += 1 #make sure it is a sucessful insertion
   end
 
   def [](key)
@@ -29,6 +35,7 @@ class SeparateChaining
 
   # Calculate the current load factor
   def load_factor #load_factor = (number of hashes being stored in the hash / the number of buckets)
+
   end
 
   # Simple method to return the number of items in the hash
