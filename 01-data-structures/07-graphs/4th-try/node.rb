7e69b4bd7ddd28eca0d4b405761c32d16b32a0da
Forrest Stone <iloveburningman@gmail.com>
2017-06-06 23:13:52 -0700

    still going at it

--- 01-data-structures/07-graphs/4th-try/node.rb
+++ 01-data-structures/07-graphs/4th-try/node.rb
@@ -1,11 +1,10 @@
-class Node
-  attr_accessor :movie
-  attr_accessor :cast
+class Node #represents an actor
+  attr_accessor :index, :name, :visited, :back_pointer, :movies
 
-  def initialize(movie, cast)
-    @movie = movie
-    @cast = cast
+  def initialize(name)
+    @name = name #the actor at center of matrix
+    @visited = false #determins if actors connection has been checked
+    @index = nil   #tracks the actors place in the @actors_array
+    @movies = []  # all the movies actor has been in.
   end
-
 end
- 
