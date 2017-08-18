10457321ef1a939d9588ffea20a1461e83f5629f
Forrest Stone <iloveburningman@gmail.com>
2017-08-18 14:04:46 -0700

    little to much and into SQL

--- 02-Algorithms/06-Improving-Complexity/improving_complexity_version_one.rb
+++ 02-Algorithms/06-Improving-Complexity/improving_complexity_version_one.rb
@@ -6,11 +6,11 @@ def poorly_written_ruby(*arrays)
 
     for val in array
       i = 0
-      if sorted_array == nil
+      if sorted_array == nil    #one in here
         sorted_array = [array[0]]
         i=1
       end
-      while i < sorted_array.length
+      while i < sorted_array.length  #one in this loop
         if val <= sorted_array[i]
           sorted_array.insert(i, val)
           break
