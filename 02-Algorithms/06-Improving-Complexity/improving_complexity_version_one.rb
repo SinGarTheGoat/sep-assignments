cc793b771d37e4c6bbf96077045ebd73d4d420b9
Forrest Stone <iloveburningman@gmail.com>
2018-01-07 14:01:10 -0800

    optimization done

--- 02-Algorithms/06-Improving-Complexity/improving_complexity_version_one.rb
+++ 02-Algorithms/06-Improving-Complexity/improving_complexity_version_one.rb
@@ -2,16 +2,16 @@
 
  def poorly_written_ruby(*arrays)
    combined_array = []
-   arrays.each do |array|
+   arrays.each do |array|   
     combined_array += array
     # #or use
     # combined_array.concat(array)
    end
 
-   combined_array.sort!
+   combined_array.sort! # big O of n^2
 
    combined_array
  end
- 
+
 
 p  poorly_written_ruby([2,3,5],[1,5,3],[9,4,2])
